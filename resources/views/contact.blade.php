1cd6ce6ccf3a89bddf47ded21116025a48d97b5a
Muhammed_Zakaria <zakaria.dev0110@gmail.com>
2026-08-02 21:29:08 +0300

    Created some routes and made the pages dynamic

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>We are in Contact page.</h1>
+</x-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-layout> {{-- the name coming after "x-" is the file name --}}
+    <h1>We are in Home page.</h1>
+</x-layout>
